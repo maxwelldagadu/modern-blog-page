a818aacf6c3afc267cbf67ef35cab31585f213dd
Maxwell Dagadu <maxwelldagadu02@gmail.com>
2026-08-27 13:59:54 +0000

    set-up the bolg page styling

--- app/(shared-layout)/blog/page.tsx
+++ app/(shared-layout)/blog/page.tsx
@@ -2,8 +2,12 @@
 
 import { useQuery } from "convex/react";
 import { api } from "@/convex/_generated/api";
-import { Card,CardHeader,CardTitle,CardDescription,CardContent } from "@/components/ui/card";
+import { Card,CardHeader,CardTitle,CardDescription,CardContent, CardFooter } from "@/components/ui/card";
 import Image from 'next/image';
+import Link from 'next/link'
+import { buttonVariants } from "@/components/ui/button";
+import { cn } from "@/lib/utils";
+
 
 
 export default function AllBlogs(){
@@ -15,21 +19,38 @@ export default function AllBlogs(){
         <h2 className="text-4xl mx-auto font-extrabold tracking-tight sm:text-5xl">
           Our Blog
         </h2>
-        <p className="pt-4 max-w-2xl text-muted-foreground">
+        <p className="pt-4 text-muted-foreground">
           Insight, thoughts and trends from our team
         </p>
       </div>
 
-      <div className="grid p-6 gap-x-2 gap-y-6 md:grid-cols-2 lg:grid-cols-3">
+      <div className="grid p-6 gap-x-5 gap-y-6 md:grid-cols-2 lg:grid-cols-3">
         {userBlogs?.map((blog) => {
          return (
-            <Card key={blog._id} className="relative h-50 w-full hover:cursor-pointer rounded-xl  border border-muted-foreground">
-             <Image src='https://cdn.britannica.com/22/19222-050-2267F357/Bob-Marley.jpg'
-              fill 
-              alt="Bob Marley"
-              placeholder='blur'
-              blurDataURL="data"
-              />
+            <Card key={blog._id} className="py-0">
+              <div className="relative h-50 w-full">
+                <Image src='https://cdn.britannica.com/22/19222-050-2267F357/Bob-Marley.jpg'
+                  fill 
+                  alt="Bob Marley"
+                  className="object-cover rounded-t-lg"
+                  sizes="300"
+                />
+              </div>
+              <CardContent>
+                <Link href={`/blog/${blog._id}`}>
+                  <h2 className="text-2xl font-bold hover:text-blue-500">
+                    {blog.title}
+                  </h2>
+                </Link>
+                <p className="text-muted-foreground font-medium line-clamp-3">
+                  {blog.body}
+                </p>
+              </CardContent>
+              <CardFooter>
+                <Link href={`/blog/${blog._id}`} className={cn(buttonVariants({className:'w-full'}))}>
+                  Read More
+                </Link>
+              </CardFooter>
             </Card>
           )
         })}
