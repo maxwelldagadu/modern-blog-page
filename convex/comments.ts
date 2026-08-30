89b57d07af907fb03839168d41b98593eb4b16bc
Maxwell Dagadu <maxwelldagadu02@gmail.com>
2026-08-30 23:38:16 +0000

    implemented create blog functionality

--- convex/comments.ts
+++ convex/comments.ts
@@ -1,7 +1,8 @@
-import { v } from "convex/values";
-import { query } from "./_generated/server";
+import { ConvexError, v } from "convex/values";
+import { mutation, query } from "./_generated/server";
+import { authComponent } from "./auth";
 
-
+// Get blog commenst
 export const getComments = query({
   args: {postId:v.id('blogs')},
   handler: async (ctx,args) => {
@@ -9,4 +10,24 @@ export const getComments = query({
     .filter((cmt) => cmt.eq(cmt.field('postId'),args.postId)).order('desc').collect();
     return comments;
   } 
+})
+
+// Create blog comments
+export const createComment = mutation({
+  args: {
+    body: v.string(),
+    postId: v.id('blogs'),
+  },
+  handler: async(ctx,args) => {
+    const user = await authComponent.safeGetAuthUser(ctx);
+      
+    if(!user) throw new ConvexError ("You're not authenticated");
+        
+    await ctx.db.insert('comments',{
+      body:args.body,
+      author: user._id,
+      postId: args.postId,
+      authorName: user.name
+    });
+  }
 })
